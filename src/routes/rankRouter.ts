b937222d3c7a9332e9794898d87bf0829b5395e9
NUMBART <subhrangshu.pandey.hit@gmail.com>
2022-04-16 17:01:03 +0530

    fixed home not loading issue

--- src/routes/rankRouter.ts
+++ src/routes/rankRouter.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 const rankRouter = express.Router();
 import * as rankController from '../controllers/rankController';
-// import html from '../../frontend/leaderboard.html';
+let __dirname = process.cwd();
 
 rankRouter.post('/friends', (req, res) => {
   rankController.getFriends(req, res);
@@ -11,12 +11,8 @@ rankRouter.get('/global', (req, res) => {
   rankController.getGlobal(req, res);
 });
 
-// rankRouter.get('/g1', (req, res) => {
-//   res.send('requi');
-// });
-
 rankRouter.get('/', function (req, res) {
-  res.sendFile('/Users/subhrangshu/Documents/Nodejs/leetboard/frontend/leaderboard.html');
+  res.sendFile(__dirname + '/frontend/leaderboard.html');
 });
 
 export default rankRouter;
